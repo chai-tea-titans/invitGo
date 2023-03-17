6da622cd7aa00a49b14ceeb15523f5b2021b6464
delvis Soto <delvissoto99@icloud.com>
2023-03-17 02:11:47 -0500

    nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider from 'next-auth/providers/google'
 
 
 export default NextAuth({
@@ -10,5 +10,6 @@ export default NextAuth({
       clientId:process.env.GOOGLE_ID,
       clientSecret:process.env.GOOGLE_SECRET
     }),
+    // ...add more providers here
   ],
 })

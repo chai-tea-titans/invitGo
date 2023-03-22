95a8868cdb90e3be3be73e0d0fc89d988d3704b4
delvis Soto <delvissoto99@icloud.com>
2023-03-22 10:34:29 -0500

    models relationships

--- server/database/Index.js
+++ server/database/Index.js
@@ -7,13 +7,18 @@ const Expense = require('./Expense');
 const Video = require('./Video');
 const CoolScore = require('./CoolScore');
 
-// user associations
-
-
-// calendar associations
-
-
-// anything association
+// Define models and relationships here
+User.hasOne(CoolScore);
+CoolScore.belongsTo(User);
+User.hasMany(Event);
+User.hasMany(Video);
+User.hasMany(Expense);
+Expense.belongsTo(Event);
+Event.belongsTo(User);
+Event.hasMany(Expense);
+Event.hasMany(Video);
+Video.belongsTo(User);
+Video.belongsTo(Event);
 
 
 

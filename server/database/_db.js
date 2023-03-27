a2d0b9181538bec3dc3520d1c48f0c16457a5cb1
CarlosZ123 <luisczepeda@gmail.com>
2023-03-26 20:23:10 -0500

    Video to popup window updates ; event assoc needed

--- server/database/_db.js
+++ server/database/_db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 //set local db name to 'grace_shopper' to avoid errors when syncing
+const { DATABASE_URL } = process.env;
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/invitego', { logging: false });
 
 // Test the database connection
@@ -11,7 +12,19 @@ db.authenticate()
     console.error('Unable to connect to the database:', err);
   });
 
+
+
+  const sequelize = new Sequelize(DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: process.env.NODE_ENV === 'production'
+    }
+  });
+
+
 module.exports = db;
+module.exports = sequelize;
+
 
 
 //OLD CODE ---- anything that needs to be kept?

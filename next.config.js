58fb0333866cfb3d1066174c58996f5e3a5630cc
delvis Soto <delvissoto99@icloud.com>
2023-03-28 10:36:19 -0500

    commit trying video, and users , payments

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withTM= require('next-transpile-modules')(['@square/web-sdk', 'react-square-web-payments-sdk'])
+
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 }
 
-module.exports = nextConfig
+
+
+module.exports = {
+  nextConfig
+}

b26a54140ce01a1df3ff39c29ae00dad686c2f7a
CarlosZ123 <luisczepeda@gmail.com>
2023-03-28 19:35:11 -0500

    comment out cloud code in _db.js

--- server/database/_db.js
+++ server/database/_db.js
@@ -1,56 +1,56 @@
-// const Sequelize = require('sequelize');
-// const { DATABASE_URL } = process.env;
-// const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/invitego', { logging: false });
+const Sequelize = require('sequelize');
+const { DATABASE_URL } = process.env;
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/invitego', { logging: false });
 
-// // Test the database connection
-// db.authenticate()
-//   .then(() => {
-//     console.log('Database connection successful');
-//   })
-//   .catch((err) => {
-//     console.error('Unable to connect to the database:', err);
-//   });
-
-
-
-//   const sequelize = new Sequelize(DATABASE_URL, {
-//     dialect: 'postgres',
-//     dialectOptions: {
-//       ssl: process.env.NODE_ENV === 'production'
-//     }
-//   });
+// Test the database connection
+db.authenticate()
+  .then(() => {
+    console.log('Database connection successful');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 
-// module.exports = db;
-// module.exports = sequelize;
+
+  const sequelize = new Sequelize(DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: process.env.NODE_ENV === 'production'
+    }
+  });
+
+
+module.exports = db;
+module.exports = sequelize;
 
 // Pre-cloud code ^^^
 /////////////////////////////////////////////////
-const Sequelize = require('sequelize');
-const { postgres, DB_NAME, DB_HOST, DB_PORT } = process.env;
+// const Sequelize = require('sequelize');
+// const { postgres, DB_NAME, DB_HOST, DB_PORT } = process.env;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: true,
-  },
-});
+// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+//   host: DB_HOST,
+//   port: DB_PORT,
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: true,
+//   },
+// });
 
-// Test the database connection
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Database connection successful");
-    console.log("Database connection successful");
-  })
-  .catch(err => {
-    console.error("Unable to connect to the database:", err);
+// // Test the database connection
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log("Database connection successful");
+//     console.log("Database connection successful");
+//   })
+//   .catch(err => {
+//     console.error("Unable to connect to the database:", err);
 
-  });
+//   });
 
-module.exports = db;
+// module.exports = db;
 
 //OLD CODE ---- anything that needs to be kept?
 // const Sequelize = require('sequelize')
@@ -84,4 +84,4 @@ module.exports = db;
   //   console.error('Unable to connect to the database:', err);
   // });
 
-module.exports = sequelize;
+// module.exports = sequelize;

492b191cefa0b895d60c33967ccfd056b52f744d
CarlosZ123 <luisczepeda@gmail.com>
2023-03-29 17:50:52 -0500

    index.js routing not needed

--- pages/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-router.use("/user", require("./users"));
-router.use("/calendar", require("./calendar"));
-router.use("/video", require("./video"));
-router.use("/spending", require("./spending"));
-
-//add additional model routes here as you create them!
-
-module.exports = router;

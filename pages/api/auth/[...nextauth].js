9dfe6cc286e1294029e3f7c4a35a2cc8c134c36b
CarlosZ123 <luisczepeda@gmail.com>
2023-03-30 13:39:46 -0500

    nextAuth fixes for deployment

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,6 +5,7 @@ import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
   // Configure one or more authentication providers
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId:'26677468679-gtoes9rie186g9nsa8l8quu52obq4hoa.apps.googleusercontent.com',

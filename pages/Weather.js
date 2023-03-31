c65e0495fc043c9ea67cc370e7dfd4240f66040d
BarTotti <lalipop123@outlook.com>
2023-03-31 14:19:46 -0400

    weather added

--- pages/Weather.js
+++ pages/Weather.js
@@ -1,35 +1,47 @@
 import React, { useState, useEffect } from "react";
 
-const API_KEY = "a2a12f5e1af70c2d839de826515c5e1e";
-const API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather";
-
 function Weather() {
   const [weatherData, setWeatherData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(position => {
-      const { latitude, longitude } = position.coords;
-
-      fetch(`${API_ENDPOINT}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
-        .then(response => response.json())
-        .then(data => {
-          setWeatherData(data);
-          setLoading(false);
-        })
-        .catch(error => console.log(error));
-    });
+    fetch("https://ipapi.co/json/")
+      .then(response => response.json())
+      .then(data => {
+        setLocation(data);
+        const { latitude, longitude } = data;
+        const API_KEY = "a2a12f5e1af70c2d839de826515c5e1e";
+        const API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=imperial`;
+        fetch(API_ENDPOINT)
+          .then(response => response.json())
+          .then(data => {
+            setWeatherData(data);
+          })
+          .catch(error => console.log(error));
+      })
+      .catch(error => console.log(error));
   }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div>
-      <h1>{weatherData.name}</h1>
-      <p>Current Temperature: {weatherData.main.temp}</p>
-      <p>Weather Description: {weatherData.weather[0].description}</p>
+      {location && (
+        <div>
+          <h2>
+            Location: {location.city}, {location.region},{" "}
+            {location.country}
+          </h2>
+        </div>
+      )}
+      {weatherData && (
+        <div>
+          <p>{weatherData.main.temp}°F</p>
+          <p>{weatherData.weather[0].description}</p>
+          <img
+            src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+            alt="weather icon"
+          />
+        </div>
+      )}
     </div>
   );
 }

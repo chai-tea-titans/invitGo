ae55516c3dc2b51b0411a1675753b23ab52ba0ea
Delvis Soto Soto <delvissoto99@icloud.com>
2023-03-31 13:35:59 -0500

    Update _db.js

--- server/database/_db.js
+++ server/database/_db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const { DATABASE_URL } = process.env;
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:123@localhost:5432/invitego', { logging: false });
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/invitego', { logging: false });
 const pg = require('pg');
 
 

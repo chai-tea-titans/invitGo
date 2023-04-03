55083b556039f87dfef764ebb227507f56fdeade
CarlosZ123 <118822229+CarlosZ123@users.noreply.github.com>
2023-04-03 12:25:40 -0500

    update api/video.js

--- pages/api/video.js
+++ pages/api/video.js
@@ -1,50 +1,86 @@
-// const express = require('express');
-// const router = express.Router();
-// const { uploadVideo } = require('../../server/database/googleCloudStore');
-// // const { Event} = require('../database/Event');
-// const { Video } = require('../../server/database/Index')
-// const { Storage } = require('@google-cloud/storage');
+const { Video } = require('../../server/database/Index')
+const { Storage } = require('@google-cloud/storage');
+import nextConnect from 'next-connect';
+import multer from 'multer';
+import path from 'path';
+const { uploadVideo } = require('../../server/database/googleCloudStore');
+const os = require('os');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
 
 
-// const storage = new Storage({
-//   projectId: 'invitegoreflecting-surf-380816',
-//   keyFilename: './secrets/reflecting-surf-380816-251f309b734b.json',
+const storage = new Storage({
+  projectId: 'reflecting-surf-380816', 
+  keyFilename: path.join(process.cwd(), 'secrets/reflecting-surf-380816-251f309b734b.json'),
+
+});
+
+// // const upload = multer({ storage: multer.memoryStorage() });
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, os.tmpdir());
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, Date.now() + '-' + file.originalname);
+//     },
+//   }),
 // });
 
-// router.post('/upload-video', async (req, res) => {
-//   try {
-//     if (!req.files || !req.files.file) {
-//       return res.status(400).send('No file uploaded');
-//     }
+// Create a Multer instance to handle file uploads
+const upload = multer({
+  storage: multer.memoryStorage(), // Save the uploaded file to memory
+  limits: {
+    fileSize: 10 * 1024 * 1024, // Limit file size to 10MB
+  },
+});
 
-//     const file = req.files.file;
-//     const blob = file.data;
-//     const fileName = `${Date.now()}.webm`;
+const handler = nextConnect()
+  .use(upload.single('file'))
+  .post(async (req, res) => {
+    try {
+      console.log('start of try')
+      if (!req.file || !req.file.buffer) { 
+        return res.status(400).send('No file uploaded or incomplete request');
+      }
 
-//     const bucketName = 'invitego';
-//     const bucket = storage.bucket(bucketName);
-//     const options = { resumable: false };
+      const file = req.file;
+      const fileName = `${Date.now()}.webm`;
+      const tempFilePath = file.path;
 
-//     await bucket.upload(blob, {
-//       destination: fileName,
-//       metadata: {
-//         contentType: file.mimetype,
-//       },
-//     });
+      const bucketName = 'invitego';
+      const bucket = storage.bucket(bucketName);
 
-//     const publicUrl = `https://storage.googleapis.com/${bucketName}/${fileName}}`;
+console.log("I am here")
+      
+await bucket.upload(file.buffer, {
+        destination: fileName,
+        metadata: {
+          contentType: file.mimetype,
+        },
+      });
+
+      const publicUrl = await uploadVideo(req.file.buffer, fileName);
+      console.log("publicUrl value: ", publicUrl);
+
+      // Delete the temporary file
+      await fsPromises.unlink(tempFilePath);
+
     
-//     // Save the video URL and file data to the database
-//     const video = await Video.create({
-//       url: publicUrl,
-//       file: blob,
-//     });
+    // Save the video URL and file data to the database
+    const video = await Video.create({
+      url: publicUrl,
+    });
 
-//     res.json({ videoUrl: publicUrl });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Error uploading video');
-//   }
-// });
+    console.log('Video object:', video);
 
-// module.exports = router;
+
+    res.json({ videoUrl: publicUrl });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(`Error uploading video: ${error.message}`);
+  }
+
+});
+
+export default handler;

6a026ecd7ef62fd699363a06c17e0a6895120a77
CarlosZ123 <luisczepeda@gmail.com>
2023-04-04 10:57:06 -0500

    test 8

--- pages/api/store/store.js
+++ pages/api/store/store.js
@@ -3,7 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import CalendarSlice from "./testSlice";
 import notificationsSlice from "./notificationsSlice";
 import SpendingSlice from "./spendingSlice";
-import videoslice from "./videoslice";
+import {videoslice} from "./videoslice";
 
 const store = configureStore({
   reducer: {

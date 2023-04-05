f60799f59410ea5f1c62e10b71ef7a5cb65eaa8f
CarlosZ123 <luisczepeda@gmail.com>
2023-04-05 06:26:54 -0500

    2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
-import Video from '../component/videoooo'
+import Video from '../component/Video'
 import Head from 'next/head'
 import Link from 'next/link'
 
